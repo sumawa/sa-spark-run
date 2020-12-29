62c453c092505c8e1539716172250773e5a8f4aa
Sumant Awasthi <awasthi.sumant@gmail.com>
2020-12-29 07:18:11 +0530

    clean up unused code

--- sa-spark-run/src/main/scala/com/sa/sparkrun/handlers/yarn/LocalResourceHelper.scala
+++ sa-spark-run/src/main/scala/com/sa/sparkrun/handlers/yarn/LocalResourceHelper.scala
@@ -37,38 +37,4 @@ object LocalResourceHelper{
         EitherT.leftT[F,Map[String,LocalResource]] (ex.getMessage)
 
     }}
-//    fromPath(new Path(app.jarPath)).fold( msg => EitherT.left[Map[String,LocalResource]](F.delay(msg))
-//      , localResourceMap => )
-
-
-//    fromPath(new Path(app.jarPath))
-//      .map { res =>
-//        Map(
-//          "__app__.jar" -> res
-//        )
-//      }
-
-  import cats.syntax.functor._
-  import cats.syntax.flatMap._
-
-//  def fromPath[F[_]](path: Path)(implicit F: ConcurrentEffect[F]): EitherT[F,String,LocalResource] =
-//    for {
-////      _  <- F.delay(println(s"creating local resource from ${path.toString}"))
-//      lr <- createFromPath(path)
-//    } yield lr
-//
-//  private def createFromPath[F[_]](path: Path)(implicit F: ConcurrentEffect[F]): EitherT[F,String,LocalResource] =
-//    scala.util.Try(FileSystem.get(hadoopCnf).getFileStatus(path)) match {
-//      case scala.util.Success(jarStat) => EitherT.rightT[F,String] {
-//        val resource = Records.newRecord(classOf[LocalResource])
-//        resource.setResource(ConverterUtils.getYarnUrlFromPath(path))
-//        resource.setSize(jarStat.getLen)
-//        resource.setTimestamp(jarStat.getModificationTime)
-//        resource.setType(LocalResourceType.FILE)
-//        resource.setVisibility(LocalResourceVisibility.APPLICATION)
-//        resource
-//      }
-//      case scala.util.Failure(ex) => EitherT.leftT[F,LocalResource] (ex.getMessage)
-//  }
-
 }
